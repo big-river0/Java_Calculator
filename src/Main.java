b0341479dd463eb134a86a82a3bae218a65c2afa
ptk <ppttkk9900@gmail.com>
2025-10-01 14:42:17 +0900

    플러스 객체만 남겨둠

--- src/Main.java
+++ src/Main.java
@@ -7,23 +7,5 @@ public class Main {
         calc_add.setNumbers(5,3);
         calc_add.add();
         calc_add.displayResult("+");
-
-        Calculator calc_subtract = new Calculator();
-
-        calc_subtract.setNumbers(5,3);
-        calc_subtract.subtract();
-        calc_subtract.displayResult("-");
-
-        Calculator calc_multiply = new Calculator();
-
-        calc_multiply.setNumbers(5,3);
-        calc_multiply.multiply();
-        calc_multiply.displayResult("*");
-
-        Calculator calc_divide = new Calculator();
-
-        calc_divide.setNumbers(5,3);
-        calc_divide.divide();
-        calc_divide.displayResult("/");
     }
 }

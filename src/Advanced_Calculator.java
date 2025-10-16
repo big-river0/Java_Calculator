4f63f681d75fa711e3cc44eabc3af14e536f250d
ptk <ppttkk9900@gmail.com>
2025-10-16 10:31:40 +0900

    Advanced_CalculatorTest 단위테스트 개발 완료

--- src/Advanced_Calculator.java
+++ src/Advanced_Calculator.java
@@ -1,13 +1,13 @@
 public class Advanced_Calculator extends Calculator {
-
-
+    // 계산기 v2.0 개발
+    // 이슈: https://github.com/big-river0/Java_Calculator/issues
 
     // 곱셈 연산자로 두수 곱하기 완료
     //https://github.com/big-river0/Java_Calculator/issues/2
     @Override
     public int multiply() {
         result = num1 * num2;
-        return 0;
+        return result;
     }
 
     //나눗셈 연산자로 두 수 나누기 완료
@@ -15,6 +15,6 @@ public class Advanced_Calculator extends Calculator {
     @Override
     public int divide() {
         result = num1 / num2;
-        return 0;
+        return result;
     }
 }
